d247a755b549a0394ff884cb984beed7f1753fa9
Alexander Agnarson <alexander.agnarson@gmail.com>
2019-07-29 14:40:22 +0200

    Fix FA5 admin links

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -543,7 +543,7 @@ Kirki::add_field( 'stylizer_theme', array(
 	'label'			=> esc_html__( 'Create Social Links', 'stylizer' ),
 	'description'	=> esc_html__( 'Create and organize your social links', 'stylizer' ),
 	'section'		=> 'social',
-	'tooltip'		=> esc_html__( 'Font Awesome names:', 'stylizer' ) . ' <a href="https://fontawesome.com/v4.7.0/icons/" target="_blank"><strong>' . esc_html__( 'View All', 'stylizer' ) . ' </strong></a>',
+	'tooltip'		=> esc_html__( 'Font Awesome names:', 'stylizer' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'stylizer' ) . ' </strong></a>',
 	'row_label'		=> array(
 		'type'	=> 'text',
 		'value'	=> esc_html__('social link', 'stylizer' ),
@@ -560,7 +560,7 @@ Kirki::add_field( 'stylizer_theme', array(
 		'social-icon'	=> array(
 			'type'			=> 'text',
 			'label'			=> esc_html__( 'Icon Name', 'stylizer' ),
-			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'stylizer' ) . ' <a href="https://fontawesome.com/v4.7.0/icons/" target="_blank"><strong>' . esc_html__( 'View All', 'stylizer' ) . ' </strong></a>',
+			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'stylizer' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'stylizer' ) . ' </strong></a>',
 			'default'		=> 'fa-',
 		),
 		'social-link'	=> array(
