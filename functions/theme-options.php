210e6180472baca9348e54a5c7eb6048c0c859f3
Alexander Agnarson <alexander.agnarson@gmail.com>
2020-03-23 21:34:00 +0100

    Remove responsive layout on/off option, regenerate .pot file

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -81,15 +81,6 @@ Kirki::add_section( 'styling', array(
 /*  Add Fields
 /* ------------------------------------ */
 
-// General: Responsive Layout
-Kirki::add_field( 'stylizer_theme', array(
-	'type'			=> 'switch',
-	'settings'		=> 'responsive',
-	'label'			=> esc_html__( 'Responsive Layout', 'stylizer' ),
-	'description'	=> esc_html__( 'Mobile and tablet optimizations (responsive.css)', 'stylizer' ),
-	'section'		=> 'general',
-	'default'		=> 'on',
-) );
 // General: Mobile Sidebar
 Kirki::add_field( 'stylizer_theme', array(
 	'type'			=> 'switch',
